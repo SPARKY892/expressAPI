5cb5350e0caff5bd832b801e3c2453abb08058d8
Mark <marksmellie892@gmail.com>
2021-03-09 13:35:48 +0000

    Added signin and signup routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const bodyParser = require('body-parser');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.sc82b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
@@ -16,8 +22,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
